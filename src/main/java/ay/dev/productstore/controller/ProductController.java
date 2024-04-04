b814d5bc48539630cc461a8b2efe496adbc68d7d
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-04 07:31:04 +0200

    controller validation i.e. UI

--- src/main/java/ay/dev/productstore/controller/ProductController.java
+++ src/main/java/ay/dev/productstore/controller/ProductController.java
@@ -3,11 +3,16 @@ package ay.dev.productstore.controller;
 import ay.dev.productstore.models.Product;
 import ay.dev.productstore.models.ProductDto;
 import ay.dev.productstore.repo.ProductRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -31,5 +36,26 @@ public class ProductController {
         ProductDto productDto = new ProductDto();
         model.addAttribute("productDto", productDto);
         return "products/CreateProduct";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(
+            @Valid @ModelAttribute ProductDto productDto,
+            BindingResult result
+    ) {
+        if (productDto.getImageFile().isEmpty()) {
+            result.addError(
+                    new FieldError(
+                            "productDto",
+                            "imageFile",
+                            "The image file is required"
+                    )
+            );
+        }
+
+        if (result.hasErrors()) {
+            return "products/CreateProduct";
+        }
+        return "redirect:/products";
     }
 }
